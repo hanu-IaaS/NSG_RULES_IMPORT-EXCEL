129d3f2e8e029c9268c8ef75d5bff0ed9ce8c72e
Think Azure Think hanu <40360909+hanu-IaaS@users.noreply.github.com>
2018-06-28 17:50:22 +0530

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,5 @@
 # Clear all variables
+#Login-AzureRmAccount
 Remove-Variable * -ErrorAction SilentlyContinue; 
 $progressPreference = 'silentlyContinue'
 $WarningPreference  = 'silentlyContinue' 
@@ -8,12 +9,12 @@ $module = Get-Module -Name ImportExcel
 if ($module -eq $null) { Write-Host "Module ImportExcel is not installed. Installing.." -ForegroundColor Yellow;
 Install-Module -Name ImportExcel -RequiredVersion 4.0.8 -Force -WarningAction SilentlyContinue }
 
-$rg_name   = "NSG"
-$location  = "eastus"
+$rg_name   = "nsg"
+$location  = "west central us"
 $ExcelFile = "https://raw.githubusercontent.com/hanu-IaaS/NSG_RULES_IMPORT-EXCEL/NSG-Rules-v0.2.xlsx"
 
 # NSG name array
-$nsg_name = @("NSG1","NSG2","NSG3")
+$nsg_name = @("adr-web-dmz-nsg01","adr-web-dmz-nsg02","adr-web-dmz-nsg03","adr-web-dmz-nsg04","adr-app-dmz-nsg01")
 
 # List VNET
 $vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $rg_name
@@ -28,18 +29,20 @@ $addrprefix = (Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet).Addr
 $NSGs = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $rg_name
 
 # Create NSG in each subnet
-Foreach($nsg in $nsg_name) {New-AzureRmNetworkSecurityGroup -ResourceGroupName $rg_name -Name $nsg -Location $location}
+Foreach($nsg in $nsg_name) {New-AzureRmNetworkSecurityGroup -ResourceGroupName $rg_name -Name $nsg -Location $location -Force  }
 
 # Attach NSG to each subnet
-for($i=0; $i -lt $NSGs.Length; $i++){
+
+for($i=0; $i -lt $NSGs.Length; $i++){
 Set-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnets[$i].Name -AddressPrefix $addrprefix[$i] -NetworkSecurityGroup $NSGs[$i] | Set-AzureRmVirtualNetwork
 Write-Host $NSGs[$i].Name "attached to" $subnets[$i].Name -ForegroundColor Green }
 
 #Import the excel with the rules and apply on each NSG
-for($n=0; $n -lt $NSGs.Length; $n++) {
-Write-Host "Importing NSG rules for" $NSGs[$n].Name -ForegroundColor Green
-$Rule = Import-Excel -Path $ExcelFile -WorksheetName $subnets[$n].Name
-Write-Host "Adding rules to" $NSGs[$n].Name "for Subnet" $subnets[$n].Name -ForegroundColor Green
+for($n=0; $n -lt $nsg_name.Length; $n++) {
+Write-Host "Importing NSG rules for" $nsg_name[$n].Name -ForegroundColor Green
+$Rule = Import-Excel -Path $ExcelFile -WorksheetName $subnets[$n].Name}
+
 for($r=0; $r -lt $Rule.Length; $r++) {
-Add-AzureRmNetworkSecurityRuleConfig -Name $Rule[$r].ruleName  -NetworkSecurityGroup $NSGs[$n] -Protocol $Rule[$r].protocol -SourcePortRange $Rule[$r].sourcePort -DestinationPortRange $Rule[$r].destinationPort -SourceAddressPrefix $Rule[$r].sourcePrefix -DestinationAddressPrefix $Rule[$r].destinationPrefix -Access $Rule[$r].access -Priority $Rule[$r].priority -Direction $Rule[$r].direction -Description $Rule[$r].description | Set-AzureRmNetworkSecurityGroup 
-}}
+Write-Host "Adding rules to" $nsg_name[$n].Name "for Subnet" $subnets[$n].Name -ForegroundColor Green
+Add-AzureRmNetworkSecurityRuleConfig -Name $Rule[$r].ruleName  -NetworkSecurityGroup $nsg_name[$n] -Protocol $Rule[$r].protocol -SourcePortRange $Rule[$r].sourcePort -DestinationPortRange $Rule[$r].destinationPort -SourceAddressPrefix $Rule[$r].sourcePrefix -DestinationAddressPrefix $Rule[$r].destinationPrefix -Access $Rule[$r].access -Priority $Rule[$r].priority -Direction $Rule[$r].direction -Description $Rule[$r].description | Set-AzureRmNetworkSecurityGroup 
+}
